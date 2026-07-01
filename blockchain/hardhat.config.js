325053c77f2b59b411371657f056a2fd561341b2
Mirna Pušeljić <puseljicmirna@gmail.com>
2026-07-02 01:32:56 +0200

    deploy

--- blockchain/hardhat.config.js
+++ blockchain/hardhat.config.js
@@ -17,5 +17,9 @@ module.exports = {
     localhost: {
       url: "http://127.0.0.1:8546",
     },
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL,
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
   },
 };
